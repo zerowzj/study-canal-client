45d2c7bf58cf5633f2baeef9be5e585f68ec5093
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-03-15 11:10:02 +0800

    更新代码

--- src/main/java/study/canal/client/support/canal/CanalClient.java
+++ src/main/java/study/canal/client/support/canal/CanalClient.java
@@ -4,24 +4,26 @@ import com.alibaba.otter.canal.client.CanalConnector;
 import com.alibaba.otter.canal.client.CanalConnectors;
 import com.alibaba.otter.canal.common.utils.AddressUtils;
 import com.alibaba.otter.canal.protocol.Message;
+import lombok.extern.slf4j.Slf4j;
 
 import java.net.InetSocketAddress;
 
+@Slf4j
 public class CanalClient {
 
     private volatile boolean running = true;
 
-    public void connector() {
+    public void connect() {
         InetSocketAddress address = new InetSocketAddress(AddressUtils.getHostIp(), 1111);
         // 创建链接
         CanalConnector connector = CanalConnectors.newSingleConnector(address, "example", "", "");
-        int batchSize = 1000;
         try {
             connector.connect();
             connector.subscribe(".*\\..*");
             connector.rollback();
+
             while (running) {
-                Message message = connector.getWithoutAck(batchSize);
+                Message message = connector.getWithoutAck(100);
                 int size = message.getEntries().size();
                 long batchId = message.getId();
                 if (batchId == -1 || size == 0) {
@@ -34,12 +36,11 @@ public class CanalClient {
                     message.getEntries();
 //                    printEntry(message.getEntries());
                 }
-                // 提交确认
+                //提交确认
                 connector.ack(batchId);
-                // connector.rollback(batchId); // 处理失败, 回滚数据
             }
         } catch (Exception ex) {
-
+            // connector.rollback(batchId); // 处理失败, 回滚数据
         }
     }
 
