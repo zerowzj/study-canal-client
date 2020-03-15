0af3856c64a05b201a12aa979f585c753f9391b0
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-03-15 11:33:23 +0800

    更新代码

--- src/main/java/study/canal/client/support/canal/CanalClient.java
+++ src/main/java/study/canal/client/support/canal/CanalClient.java
@@ -3,10 +3,12 @@ package study.canal.client.support.canal;
 import com.alibaba.otter.canal.client.CanalConnector;
 import com.alibaba.otter.canal.client.CanalConnectors;
 import com.alibaba.otter.canal.common.utils.AddressUtils;
+import com.alibaba.otter.canal.protocol.CanalEntry;
 import com.alibaba.otter.canal.protocol.Message;
 import lombok.extern.slf4j.Slf4j;
 
 import java.net.InetSocketAddress;
+import java.util.List;
 
 @Slf4j
 public class CanalClient {
@@ -14,7 +16,7 @@ public class CanalClient {
     private volatile boolean running = true;
 
     public void connect() {
-        InetSocketAddress address = new InetSocketAddress(AddressUtils.getHostIp(), 1111);
+        InetSocketAddress address = new InetSocketAddress(AddressUtils.getHostIp(), 11111);
         // 创建链接
         CanalConnector connector = CanalConnectors.newSingleConnector(address, "example", "", "");
         try {
@@ -23,9 +25,11 @@ public class CanalClient {
             connector.rollback();
 
             while (running) {
+//                log.info("sdafsad");
                 Message message = connector.getWithoutAck(100);
                 int size = message.getEntries().size();
                 long batchId = message.getId();
+//
                 if (batchId == -1 || size == 0) {
                     try {
                         Thread.sleep(1000);
@@ -33,18 +37,65 @@ public class CanalClient {
                         ex.printStackTrace();
                     }
                 } else {
-                    message.getEntries();
-//                    printEntry(message.getEntries());
+                    log.info("batch_id={}, size={}", batchId, size);
+                    printEntry(message.getEntries());
                 }
                 //提交确认
                 connector.ack(batchId);
             }
         } catch (Exception ex) {
+            ex.printStackTrace();
             // connector.rollback(batchId); // 处理失败, 回滚数据
+        }
+    }
+
+    private static void printEntry(List<CanalEntry.Entry> entrys) {
+        for (CanalEntry.Entry entry : entrys) {
+            if (entry.getEntryType() == CanalEntry.EntryType.TRANSACTIONBEGIN ||
+                    entry.getEntryType() == CanalEntry.EntryType.TRANSACTIONEND) {
+                continue;
+            }
+
+            CanalEntry.RowChange rowChage = null;
+            try {
+                rowChage = CanalEntry.RowChange.parseFrom(entry.getStoreValue());
+            } catch (Exception e) {
+                throw new RuntimeException("ERROR ## parser of eromanga-event has an error , data:" + entry.toString(),
+                        e);
+            }
+
+            CanalEntry.EventType eventType = rowChage.getEventType();
+            System.out.println(String.format("================&gt; binlog[%s:%s] , name[%s,%s] , eventType : %s",
+                    entry.getHeader().getLogfileName(), entry.getHeader().getLogfileOffset(),
+                    entry.getHeader().getSchemaName(), entry.getHeader().getTableName(),
+                    eventType));
+
+            for (CanalEntry.RowData rowData : rowChage.getRowDatasList()) {
+                if (eventType == CanalEntry.EventType.DELETE) {
+                    printColumn(rowData.getBeforeColumnsList());
+                } else if (eventType == CanalEntry.EventType.INSERT) {
+                    printColumn(rowData.getAfterColumnsList());
+                } else {
+                    System.out.println("-------&gt; before");
+                    printColumn(rowData.getBeforeColumnsList());
+                    System.out.println("-------&gt; after");
+                    printColumn(rowData.getAfterColumnsList());
+                }
+            }
+        }
+    }
+
+    private static void printColumn(List<CanalEntry.Column> columns) {
+        for (CanalEntry.Column column : columns) {
+            System.out.println(column.getName() + " : " + column.getValue() + "    update=" + column.getUpdated());
         }
     }
 
     public void shutdown() {
         running = false;
+    }
+
+    public static void main(String[] args) {
+        new CanalClient().connect();
     }
 }
