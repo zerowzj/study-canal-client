aae2a964d282a24804354b0f68b4ce1af875bee8
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-03-15 20:23:00 +0800

    更新代码

--- src/main/java/study/canal/client/support/canal/CanalClintCfg.java
+++ src/main/java/study/canal/client/support/canal/CanalClintCfg.java
@@ -1,6 +1,7 @@
 package study.canal.client.support.canal;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import study.canal.client.support.canal.core.CanalClient;
 
@@ -8,9 +9,17 @@ import study.canal.client.support.canal.core.CanalClient;
 @Configuration
 public class CanalClintCfg {
 
+    @Bean
+    public CanalClient canalClient2(){
+        CanalClient client = new CanalClient();
+        client.connect();
+        return client;
+    }
 
-    public CanalClient canalClient(){
-
-        return null;
+    @Bean
+    public CanalClient canalClient1(){
+        CanalClient client = new CanalClient();
+        client.connect();
+        return client;
     }
 }
